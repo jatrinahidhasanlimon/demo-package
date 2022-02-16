1e7ea795a50df57d1020abd463d62663821853da
Md. Nahid Hasan Limon <41167616+NahidHasanLimon@users.noreply.github.com>
2022-02-17 00:36:23 +0600

    register method added in service provider

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'a5b4e39c5f049ff5108b2f9caaa52ff913a9441f',
+    'name' => 'jatrinahidhasanlimon/demopackage',
+  ),
+  'versions' => 
+  array (
+    'jatrinahidhasanlimon/demopackage' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a5b4e39c5f049ff5108b2f9caaa52ff913a9441f',
+    ),
+  ),
+);
