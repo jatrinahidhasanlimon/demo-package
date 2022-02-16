5e10c17edea9d690d8873206a984e57ffa2a5b40
jatrinahidhasanlimon <nhlimon.jatri@gmail.com>
2022-02-16 18:18:40 +0600

    loadfrom routes added

--- src/DemoPackageServiceProvider.php
+++ src/DemoPackageServiceProvider.php
@@ -26,6 +26,7 @@ class DemoPackageServiceProvider extends ServiceProvider
         //
         include __DIR__.'/routes.php';
         $this->loadViewsFrom(__DIR__.'./views', 'demo');
+        $this->loadRoutesFrom(__DIR__.'/routes.php');
 
     }
 }
---
+++ src/customClass/ClassA.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace nahidhasanlimon\demo;
+
+class A
+{
+    public function greet(String $name)
+    {
+        return 'Hello ' . $name . '! How are you?';
+    }
+}
